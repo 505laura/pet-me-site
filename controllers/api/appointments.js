f08a212ad4cafcbc567248036eae5c47aeee3a17
laura <laurabranc94@gmail.com>
2022-11-18 19:48:45 +0000

    Initialise API skeleton

---
+++ controllers/api/appointments.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+router.get('/', async(req, res) => {
+});
+
+router.get('/:id', (req, res) => {
+});
+
+router.post('/', (req, res) => {
+});
+
+router.put('/:id', (req, res) => {
+});
+
+router.delete('/:id', (req, res) => {
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const routes = require('./controllers');
+const path = require('path');
+const sequelize = require('./config/connection.js');
 
+// Set up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// Connect to the database before starting the Express.js server
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+    
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
