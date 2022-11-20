a7ee827a9607dc7e7fa1806fe455cd388793db6d
gulchintory <avgulcin@gmail.com>
2022-11-20 16:22:30 +1100

    Employee login is completed

--- controllers/api/employees.js
+++ controllers/api/employees.js
@@ -1,18 +1,46 @@
 const router = require('express').Router();
+const { Employee } = require('../../models');
 
-router.get('/', async(req, res) => {
+router.post('/login', async (req, res) => {
+  try {
+    const employeeData = await Employee.findOne({ where: { email: req.body.email } });
+
+    if (!employeeData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await employeeData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.employee_id = employeeData.id;
+      req.session.logged_in = true;
+      
+      res.json({ employee: employeeData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-});
-
-router.post('/', (req, res) => {
-});
-
-router.put('/:id', (req, res) => {
-});
-
-router.delete('/:id', (req, res) => {
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-// Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const routes = require('./controllers');
 const path = require('path');
-const sequelize = require('./config/connection.js');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// Set up the Express App
+const sequelize = require('./config/connection');
+
+// TODO: Add a comment describing the functionality of this expression
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine
+const hbs = exphbs.create({ helpers });
+
+// TODO: Add a comment describing the functionality of this object
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// TODO: Add a comment describing the functionality of this statement
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(routes);
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-// Connect to the database before starting the Express.js server
-sequelize.sync().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
-    
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
