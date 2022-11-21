34e96f67d91b630ccc234d1c9dbf93dc7f5b0c6b
gulchintory <avgulcin@gmail.com>
2022-11-21 21:33:35 +1100

    TODO's removed

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
-// TODO: Add a comment describing the functionality of this expression
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -15,7 +14,6 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-// TODO: Add a comment describing the functionality of this object
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -26,7 +24,6 @@ const sess = {
   })
 };
 
-// TODO: Add a comment describing the functionality of this statement
 app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
