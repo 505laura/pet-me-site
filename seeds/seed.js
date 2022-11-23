70987586bee19c6ed93c508f4e74627dbc9a1c9f
gulchintory <avgulcin@gmail.com>
2022-11-23 21:03:13 +1100

    Pet data seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,19 @@
 const sequelize = require('../config/connection');
 const { Employee } = require('../models');
+const { Pet } = require('../models');
 
 const employeeData = require('./employeeData.json');
+const petData = require('./petData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await Employee.bulkCreate(employeeData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Pet.bulkCreate(petData, {
     individualHooks: true,
     returning: true,
   });
