08fe0584ccc97b3b726f9935d3a533928ad98be3
laura <laurabranc94@gmail.com>
2022-11-27 00:35:49 +0000

    Cleanup animal routes

--- controllers/api/animals.js
+++ controllers/api/animals.js
@@ -10,9 +10,11 @@ router.get('/:id', async(req, res) => {
     const pet = await Pet.findOne({
         where: {id: req.params.id},
         // include: []
-    });
+    }).catch(() => null);
     if (pet == null) {
-        return res.status(404);
+        return res.status(404).send({
+            message: 'Pet does not exist.'
+        });
     }
     res.json(pet);
 });
@@ -26,12 +28,12 @@ router.post('/', async(req, res) => {
 
 router.put('/:id', async(req, res) => {
     const {species, name, breed, age, colour, sex, desexed, description} = req.body;
-    const result = await Pet.update({species, name, breed, age, colour, sex, desexed, description}, {where: {id: req.params.id}});
+    await Pet.update({species, name, breed, age, colour, sex, desexed, description}, {where: {id: req.params.id}});
     res.json({success: true});
 });
 
 router.delete('/:id', async(req, res) => {
-    const result = await Pet.destroy({where: {id: req.params.id}});
+    await Pet.destroy({where: {id: req.params.id}});
     res.json({success: true});
 });
 
