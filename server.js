475d18674eb32123b90154463f9af5d5aa0c6f54
laura <laurabranc94@gmail.com>
2022-11-28 06:48:23 +0000

    Update server file

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 const sequelize = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 
-sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+sequelize.sync({ force: false }).then(async() => {
+    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
